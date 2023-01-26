7834f7b5135e43b90dbb15d49697e3bbda2d5b73
PROLCY <ckd248@naver.com>
2023-01-26 14:46:12 +0900

    first commit

---
+++ src/main/java/prolcy/wordle_maker_spring/WordleMakerSpringApplication.java
@@ -0,0 +1,13 @@
+package prolcy.wordle_maker_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordleMakerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordleMakerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prolcy/wordle_maker_spring/WordleMakerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package prolcy.wordle_maker_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleMakerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
